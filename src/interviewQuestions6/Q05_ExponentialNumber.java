67875820d6a38a691bba2c2356944fbdc8b4c82a
Hasan Çobanoğlu <hasancobanoglu93@gmail.com>
2022-04-25 17:32:17 +0300

    Merge branch 'master' of C:\yedek_hc\githup\javaPractice2022 with conflicts.

--- src/interviewQuestions6/Q05_ExponentialNumber.java
+++ src/interviewQuestions6/Q05_ExponentialNumber.java
@@ -11,6 +11,36 @@ public class Q05_ExponentialNumber {
 
     // 4, 3 --> 4^3= 4*4*4 = 64
 
+    public static void main(String[] args) {
+        System.out.println("Math ile Çözüm");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Tabanı Giriniz");
+        int sayi1= scan.nextInt();
+        scan.nextLine();           //Dummy
+        System.out.println("Üssü Giriniz");
+        int sayi2= scan.nextInt();
+        scan.nextLine();        //dummy
+        System.out.println(Math.pow(sayi1,sayi2));
+
+        System.out.println("");
+        System.out.println("***2 Çözüm for ile****");
+
+        Scanner scan1 = new Scanner(System.in);
+        System.out.println("Lutfen taban ve us giriniz :");
+        int taban = scan1.nextInt();
+        int us = scan1.nextInt();
+        int sonuc =1;
+        //    while(us !=0){
+        //        sonuc*=taban;
+        //        us--;
+        //    }
+        //    System.out.println("while ile :"+sonuc);
+        for(int i = 0; i <us;i++){
+            sonuc*=taban;
+        }
+        System.out.println("for ile :" + sonuc) ;
+    }
+    }
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
@@ -34,4 +64,3 @@ public class Q05_ExponentialNumber {
     }
 
 
-}
